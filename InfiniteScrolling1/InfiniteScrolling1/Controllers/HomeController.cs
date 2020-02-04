c2c46713c5531ea92627b667c98a36f962898210
Mark Giffard <mark@digital-loom.co.uk>
2020-02-04 22:44:13 +0000

    Added infinite scrolling

--- InfiniteScrolling1/InfiniteScrolling1/Controllers/HomeController.cs
+++ InfiniteScrolling1/InfiniteScrolling1/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using FizzWare.NBuilder;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using InfiniteScrolling1.Models;
@@ -21,6 +22,16 @@ namespace InfiniteScrolling1.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult GetTableRows(int id)
+        {
+            var batchSize = 10;
+            var offset = batchSize * (id-1);
+
+            var dtos = Builder<MyDto>.CreateListOfSize(10).All().With(x => x.Id += offset).Build().AsEnumerable();
+
+            return PartialView("_tableRows", dtos);
         }
 
         public IActionResult Privacy()
---
+++ InfiniteScrolling1/InfiniteScrolling1/Models/MyDto.cs
@@ -0,0 +1,8 @@
+﻿namespace InfiniteScrolling1.Models
+{
+    public class MyDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
